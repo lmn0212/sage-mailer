7f8f1000a19860f8e3f3fc9fbec1102250903e3d
Leonid <l.m.nykytiuk@gmail.com>
2020-02-13 16:26:48 +0200

    Add files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.testimonials-car').slick({
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 200,
+        arrows: false,
+        easing: 'easeInCubic',
+});
+});
