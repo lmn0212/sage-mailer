0738b4739e33a1fa625dc7cf84743a8178d4b690
Leonid <l.m.nykytiuk@gmail.com>
2020-02-14 19:20:34 +0200

    668 fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $('.navbar-toggler').click(function() {
+        $(this).toggleClass('open');
+    });
+
     $('.testimonials-car').slick({
         centerMode: true,
         centerPadding: '0',
@@ -14,8 +18,41 @@ $(document).ready(function(){
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     arrows: true,
+                    prevArrow:"<div class='slick-prev'><img src='../media/img/arrow.svg' alt='prev'></div>",
+                    nextArrow:"<div class='slick-next'><img src='../media/img/arrow.svg' alt='next'></div>",
                 }
             },
         ]
-});
+    });
+    $('.price-carousel').slick({
+        infinite:true,
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow:"<div class='slick-prev'></div>",
+        nextArrow:"<div class='slick-next'></div>",
+    });
+    $('.collapse')
+        .on('show.bs.collapse', function() {
+            $(this)
+                .parent()
+                .find(".arrow-collapse")
+                .css({
+                    "-webkit-transform": "rotate(90deg)",
+                    "-moz-transform": "rotate(90deg)",
+                    "transform": "rotate(90deg)"
+                });
+        })
+        .on('hide.bs.collapse', function() {
+            $(this)
+                .parent()
+                .find(".arrow-collapse")
+                .css({
+                    "-webkit-transform": "rotate(0deg)",
+                    "-moz-transform": "rotate(0deg)",
+                    "transform": "rotate(0deg)"
+                });
+        });
 });
